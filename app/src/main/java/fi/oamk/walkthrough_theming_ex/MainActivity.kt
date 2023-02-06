14aa3bc486968d90710b6ac85fc6e3df4b3d5786
hajri <c1almo00@students.oamk.fi>
2023-02-06 13:40:13 +0200

    project added

---
+++ app/src/main/java/fi/oamk/walkthrough_theming_ex/MainActivity.kt
@@ -0,0 +1,63 @@
+package fi.oamk.walkthrough_theming_ex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import fi.oamk.walkthrough_theming_ex.ui.theme.Walkthrough_theming_exTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Walkthrough_theming_exTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    //Greeting("Android")
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val appModifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+
+    ) {
+        Text(text = "My Tilte",
+            style = MaterialTheme.typography.h5,
+            modifier = appModifier
+        )
+        OutlinedTextField(value = "", onValueChange = {/*TODO*/ })
+
+        Button(onClick = { /*TODO*/ },
+            modifier = appModifier
+        ) {
+            Text(text = "Submit")
+
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Walkthrough_theming_exTheme {
+        //Greeting("Android")
+    }
+}
